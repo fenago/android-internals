247a7ed8a026c1bf77459f08e791cb80f6851590
fenago <socrates73@gmail.com>
2022-06-08 04:46:18 -0700

    Create eightman_new.mk

---
+++ resources/HIDL2/eightman_new.mk
@@ -0,0 +1,5 @@
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/cuttlefish/shared/device_framework_matrix.xml
+
+PRODUCT_PACKAGES += \
+    vendor.eightman.cpu@1.0 \
+    vendor.eightman.cpu@1.0-service
